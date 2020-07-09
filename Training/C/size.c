3718c786cb05bf9c1551edd6912787abcbdaa6eb
Uthara Keerthan <keerthanraam@gmail.com>
2020-07-10 00:17:52 +0200

    Training folder removed

--- Training/C/size.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	printf("the size of long double is: %d ",sizeof(short int));
-}
--- Training/C/trialday7.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-main()
-{
-	 int x=5;
-	printf("%d\n",x--);
-	if(x)
-		main();
-}		
